804103e669b74e133f41573e289aa6f3e6a92818
Javier Martinez <javierf.martinez.lizama@gmail.com>
2020-10-30 21:39:33 +0100

    users controller for post request completed and errors and user service created

--- controllers/ping/ping_controller.go
+++ controllers/ping/ping_controller.go
@@ -8,4 +8,3 @@ import (
 func Ping(c *gin.Context) {
 	c.String(http.StatusOK, "pong")
 }
-
--- controllers/users/users_controller.go
+++ controllers/users/users_controller.go
@@ -2,14 +2,39 @@ package users
 
 import (
 	"github.com/gin-gonic/gin"
+	"github.com/jmlgh/bookstore_users-api/domain/users"
+	"github.com/jmlgh/bookstore_users-api/services"
+	"github.com/jmlgh/bookstore_users-api/utils/errors"
 	"net/http"
 )
 
 func CreateUser(c *gin.Context) {
-	c.String(http.StatusNotImplemented, "implement me!")
+	var user users.User
+	/*bytes, err := ioutil.ReadAll(c.Request.Body)
+	if err != nil {
+		// TODO: Handle the error
+		return
+	}
+	if err = json.Unmarshal(bytes, &user); err != nil {
+		// TODO: Handle the json error
+		fmt.Println(err.Error())
+		return
+	}*/
+	// same as the above code but using Gin
+	if err := c.ShouldBindJSON(&user); err != nil {
+		restErr := errors.NewBadRequestError("invalid json body")
+		c.JSON(restErr.Status, restErr)
+		return
+	}
+
+	result, saveErr := services.CreateUser(user)
+	if saveErr != nil {
+		c.JSON(saveErr.Status, saveErr)
+		return
+	}
+	c.JSON(http.StatusCreated, result)
 }
 
 func GetUser(c *gin.Context) {
 	c.String(http.StatusNotImplemented, "implement me!")
 }
-
